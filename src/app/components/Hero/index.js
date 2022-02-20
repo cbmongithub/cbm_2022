4550498ff253f78fbf59f8f2170836ff046bbfe6
Christian Martinez <christian@mixdlabs.com>
2022-02-20 08:58:30 -0700

    Added hero interaction btn

--- src/app/components/Hero/index.js
+++ src/app/components/Hero/index.js
@@ -1,10 +1,13 @@
 import { useEffect, useRef } from 'react'
+import { FaHandPointer } from 'react-icons/fa'
 import { Canvas, useLoader, useThree, useFrame } from '@react-three/fiber'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import { RepeatWrapping, CubeTextureLoader } from 'three'
 import { Environment, Plane, OrbitControls } from '@react-three/drei'
 
 import Model from './Model'
+
+import './style.css'
 
 const CameraControls = () => {
   const {
@@ -57,35 +60,43 @@ const Hero = () => {
   texture.repeat.set(50, 50)
 
   return (
-    <Canvas
-      style={{
-        width: '100%',
-        height: '100vh',
-      }}
-      camera={{ fov: 75, position: [-180, 55, 50] }}
-      shadows
-      pixelRatio={window.devicePixelRatio}
-    >
-      <CameraControls />
-      <ambientLight color={'#fff4d9'} intensity={0.4} />
-      <pointLight
-        position={[180, 75, 75]}
-        color={'#fff4d9'}
-        intensity={0.13}
-        castShadow
-      />
-      <Plane
-        receiveShadow
-        rotation={[-Math.PI / 2, 0, 0]}
-        position={[0, -1, 0]}
-        args={[4096, 4096]}
+    <>
+      <Canvas
+        style={{
+          width: '100%',
+          height: '100vh',
+        }}
+        camera={{ fov: 75, position: [-180, 55, 50] }}
+        shadows
+        pixelRatio={window.devicePixelRatio}
       >
-        <meshPhongMaterial attach='material' map={texture} />
-      </Plane>
-      <Model />
-      <SkyBox />
-      <Environment preset='sunset' />
-    </Canvas>
+        <CameraControls />
+        <ambientLight color={'#fff4d9'} intensity={0.4} />
+        <pointLight
+          position={[180, 75, 75]}
+          color={'#fff4d9'}
+          intensity={0.13}
+          castShadow
+        />
+        <Plane
+          receiveShadow
+          rotation={[-Math.PI / 2, 0, 0]}
+          position={[0, -1, 0]}
+          args={[4096, 4096]}
+        >
+          <meshPhongMaterial attach='material' map={texture} />
+        </Plane>
+        <Model />
+        <SkyBox />
+        <Environment preset='sunset' />
+      </Canvas>
+      <div className='btn_container'>
+        <div className='bg_click'></div>
+        <div className='click'>
+          <FaHandPointer />
+        </div>
+      </div>
+    </>
   )
 }
 
